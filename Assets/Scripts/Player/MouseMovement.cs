5a726e56e946dad6dc7598c4e685d405b93354c9
Satchee <s.shastree@student.saedubai.com>
2022-02-21 20:03:42 +0400

    Added movement and camera control
    hopefully this works right

---
+++ Assets/Scripts/Player/MouseMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    public Transform player;
+    public float mouseSensitivity;
+    float xRotation;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
