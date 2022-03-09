c8bdc4732f9e1fe25025638330774c7200dd578b
Satchee <s.shastree@student.saedubai.com>
2022-03-09 13:48:51 +0400

    Fixed player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -21,7 +21,6 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
     bool canJump;
-    [HideInInspector] public bool hiding = false;
 
     private void Update()
     {
@@ -48,7 +47,7 @@ public class PlayerMovement : MonoBehaviour
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 move = (transform.right * x + transform.forward * z).normalized;
         controller.Move(move * finalSpeed * Time.deltaTime);
     }
     private void Crouch()
